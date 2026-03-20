4bfb0df6b85bd490a41c6ff0d9b3be93505ae4cc
wlegon-cloud <wlegon@essenltda.com.uy>
2026-03-20 10:03:24 -0300

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ st.markdown("""
     --negro:     #000000;
     --rojo:      #CC0000;
     --rojo-vivo: #E00000;
-    --blanco:    #FFFFFF;
+    --blanco:    #2A2A2A;
     --gris1:     #111111;
     --gris2:     #1C1C1C;
     --gris3:     #2A2A2A;
